b9215cc4f06edccb2d62640e46490f799b84a4c1
Infinite Runner <anantmishra.work@gmail.com>
2022-11-05 20:54:44 +0530

    Single Responsibility

--- src/main/java/com/example/solid/SolidApplication.java
+++ src/main/java/com/example/solid/SolidApplication.java
@@ -2,12 +2,16 @@ package com.example.solid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SolidApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SolidApplication.class, args);
+
+        ApplicationContext applicationContext = SpringApplication.run(SolidApplication.class, args);
+
+        System.out.println("Single Responsibility Principle");
     }
 
 }
